2998ce44372c7dc5d5644bb8c6c28e7b0ef5e7ea
kibriatuhin <tuinn78@gmail.com>
2021-01-28 17:56:44 +0600

    Array Practice

---
+++ src/exercises/Exercise7.java
@@ -0,0 +1,29 @@
+package exercises;
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * Created by USER on 1/28/2021.
+ */
+
+
+//test
+public class Exercise7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] myArray = new int[5];
+        System.out.printf("Enter five array list no = ");
+        for (int i=0 ; i< myArray.length ; i++){
+            myArray[i] = sc.nextInt();
+        }
+
+        System.out.printf("Enter remove index no = ");
+        int removeIndex = sc.nextInt();
+        System.out.println("Orginal Array = " + Arrays.toString(myArray));
+        for (int i= removeIndex ; i< myArray.length -1 ; i++){
+            myArray[i] = myArray[i+1];
+
+        }
+
+        System.out.println("After removing the second element: "+Arrays.toString(myArray));
+    }
+}
